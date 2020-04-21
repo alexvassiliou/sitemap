d4884e988827c69529f102db5dd28630e5872114
Alex Vassiliou <avassiliou@zendesk.com>
2020-04-21 12:55:10 +1000

    finished sitemap, can still be refactored

--- main/main.go
+++ main/main.go
@@ -1,33 +1,119 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 
-	"github.com/alexvassiliou/gophercises/sitemap"
+	"github.com/alexvassiliou/gophercises/link"
 )
 
-func main() {
-	url := flag.String("url", "https://www.calhoun.io", "provide the url for the root page to map")
-	flag.Parse()
+const xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
 
-	counter := 0
-	var links sitemap.Links
-	fmt.Println(counter)
-	links["first"] = sitemap.GetLinks(*url, &counter)
-	fmt.Println(counter)
-	// links := sitemap.GetLinks(*url)
-
-	// open each link and repeat the process
-	// dont hit duplicates
+type loc struct {
+	Value string `xml:"loc"`
 }
 
-// function to print and test link array
-func test(list []string) {
-	fmt.Println("-----------------")
-	for _, l := range list {
-		fmt.Println(l)
+type urlset struct {
+	Urls  []loc  `xml:"url"`
+	Xmlns string `xml:"xmlns,attr"`
+}
+
+func main() {
+	urlFlag := flag.String("url", "https://gophercises.com", "provide the url for the root page to map")
+	maxDepth := flag.Int("depth", 3, "maximum number of links deep to traverse")
+	flag.Parse()
+
+	pages := bfs(*urlFlag, *maxDepth)
+	toXML := urlset{
+		Xmlns: xmlns,
 	}
-	fmt.Println(len(list))
-	fmt.Println("-----------------")
+	for _, page := range pages {
+		toXML.Urls = append(toXML.Urls, loc{page})
+	}
+	fmt.Print(xml.Header)
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", "  ")
+	if err := enc.Encode(toXML); err != nil {
+		panic(err)
+	}
+	fmt.Println()
+}
+
+func bfs(urlStr string, maxDepth int) []string {
+
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: struct{}{},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		for url := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				nq[link] = struct{}{}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen))
+	for url := range seen {
+		ret = append(ret, url)
+	}
+	return ret
+}
+
+func hrefs(r io.Reader, base string) []string {
+	links, _ := link.ParseLinks(r)
+	var ret []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			ret = append(ret, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			ret = append(ret, l.Href)
+		}
+	}
+	return ret
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
+	}
+	base := baseURL.String()
+
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
 }
